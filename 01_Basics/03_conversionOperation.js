6ea38c01fd0def929578938a6ac80abcae8fb15e
Ayush Kumar <ayushk.nitp@gmail.com>
2026-04-18 01:56:13 +0530

    more conversion

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -27,4 +27,38 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+// **************** Operations ****************
+
+let value = 3
+let negValue = -value
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " ayush"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);  // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+
+// console.log(+true); // 1
+// console.log(+""); // 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
