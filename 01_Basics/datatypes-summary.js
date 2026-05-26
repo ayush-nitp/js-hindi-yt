b748f5e1987d0b409852729630d57c2a703c3dd4
Ayush Kumar <ayushk.nitp@gmail.com>
2026-05-26 14:14:04 +0530

    modified summary

--- 01_Basics/datatypes-summary.js
+++ 01_Basics/datatypes-summary.js
@@ -33,10 +33,36 @@ const myFunction = function(){
     
 }
 
-console.log(typeof bigNumber); // bigint
-console.log(typeof outsideTemp); // object
-console.log(typeof userEmail); // undefined
-console.log(typeof myFunction);  //function(object function)
-console.log(typeof myObj);  // object
-console.log(typeof heros);  // object
-console.log(typeof anotherId);  // symbol
+// console.log(typeof bigNumber); // bigint
+// console.log(typeof outsideTemp); // object
+// console.log(typeof userEmail); // undefined
+// console.log(typeof myFunction);  //function(object function)
+// console.log(typeof myObj);  // object
+// console.log(typeof heros);  // object
+// console.log(typeof anotherId);  // symbol
+
+
+// +++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (Non Primitive)
+
+let myYoutubename = "AyushKumardotcom"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+
+let user1 = {
+    email : "user@google.com",
+    upi : "user@ibl",
+}
+
+let user2 = user1
+
+user2.email = "ayush@google.com"
+
+console.log(user1.email);
+console.log(user2.email);
