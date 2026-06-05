6ef4b7238549f79b80bd7aa92c53684da9151b5a
Ayush Kumar <ayushk.nitp@gmail.com>
2026-06-06 01:29:38 +0530

    more about objects

---
+++ 02_Basics/04_objects.js
@@ -0,0 +1,54 @@
+// const tinderUser = new Object() // singleton object
+const tinderUser = {} // non singleton object
+
+tinderUser.id = "123abc"
+tinderUser.name = "sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@google.com",
+    fullname: {
+        userfullname: {
+            firstname: "ayush",
+            lastname: "kumar"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+// const obj3 = {obj1, obj2}
+// const obj3 = Object.assign({}, obj1, obj2) // copying to obj3 else in obj1 
+
+const obj3 = {...obj1, ...obj2} // mostly used
+console.log(obj3);
+
+const users = [  // array of objects
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser)); // array datatype
+console.log(Object.values(tinderUser)); // array datatype
+console.log(Object.entries(tinderUser)); // array of array
+
+console.log(tinderUser.hasOwnProperty('isloggedIn')); // bool value
+
